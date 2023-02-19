9e7fe95f247c7b0748adce8f3d153e398122fd02
NurMuhammad1988 <nurikyorov88@gmail.com>
2023-02-19 20:39:42 +0500

    jhkfdfdsf

---
+++ app2.js
@@ -0,0 +1,82 @@
+
+
+
+// const btn1 = document.getElementById('btn1');
+// const btn2 = document.getElementById('btn2');
+// const btn3 = document.getElementById('btn3');
+// const btn4 = document.getElementById('btn4');
+// const btn5 = document.getElementById('btn5');
+// const top1 = document.getElementById('top1');
+// const button1 = document.getElementById('button1');
+
+
+// btn1.onclick = function () {
+//     top1.style.transform = 'translateX(-345px)';
+//     button1.style.transform = 'translateX(-345px)';
+
+// }
+
+// btn2.onclick = function () {
+//     top1.style.transform = 'translateX(-200px)';
+//     button1.style.transform = 'translateX(-200px)';
+
+// }
+
+// btn3.onclick = function () {
+//     top1.style.transform = 'translateX(-45px)';
+//     button1.style.transform = 'translateX(-45px)';
+
+// }
+
+// btn4.onclick = function () {
+//     top1.style.transform = 'translateX(107px)';
+//     button1.style.transform = 'translateX(107px)';
+
+// }
+
+// btn5.onclick = function () {
+//     top1.style.transform = 'translateX(255px)';
+//     button1.style.transform = 'translateX(255px)';
+
+// }
+
+
+
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
+const btn4 = document.getElementById('btn4');
+const btn5 = document.getElementById('btn5');
+const top1 = document.getElementById('top1');
+const button1 = document.getElementById('button1');
+
+
+btn1.onmouseover = function () {
+    top1.style.transform = 'translateX(-685px)';
+    button1.style.transform = 'translateX(-685px)';
+
+}
+
+btn2.onmouseover = function () {
+    top1.style.transform = 'translateX(-370px)';
+    button1.style.transform = 'translateX(-370px)';
+
+}
+
+btn3.onmouseover = function () {
+    top1.style.transform = 'translateX(-45px)';
+    button1.style.transform = 'translateX(-45px)';
+
+}
+
+btn4.onmouseover = function () {
+    top1.style.transform = 'translateX(277px)';
+    button1.style.transform = 'translateX(277px)';
+
+}
+
+btn5.onmouseover = function () {
+    top1.style.transform = 'translateX(596px)';
+    button1.style.transform = 'translateX(596px)';
+
+}
